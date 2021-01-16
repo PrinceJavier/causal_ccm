5c9fd89b49518766ba32078e46557a96ea28c1d5
Prince Joseph Javier <prince.javier@mynt.xyz>
2021-01-16 19:56:27 +0800

    made verson 0.001

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'causal_ccm',         # How you named your package folder (MyLib)
   packages = ['causal_ccm'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.01',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'implementation of convergent cross mapping by Sugihara et al (2012)',   # Give a short description about your library
   author = 'Prince Javier',                   # Type in your name
   author_email = 'othepjavier@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/PrinceJavier/causal_ccm',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/PrinceJavier/causal_ccm/archive/v_02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/PrinceJavier/causal_ccm/archive/v_001.tar.gz',    # I explain this later on
   keywords = ['causality', 'dynamical systems', 'complex systems'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
